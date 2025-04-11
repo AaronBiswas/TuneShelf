f8c55b3d6362a6fdf5dbc7e448dcc29695e3899b
Aaron Biswas <savybiswas15@gmail.com>
2025-04-12 00:02:49 +0530

    Implement useState in Signup page

--- Frontend/src/components/Signup/Signup.jsx
+++ Frontend/src/components/Signup/Signup.jsx
@@ -1,42 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+
+  const [formdata, setFormdata] = useState({
+    name:"",
+    username:"",
+    email:"",
+    password:"",
+  })
+
+  const handleChange = (e) => {
+   const {name,value} = e.target;
+    setFormdata({
+      ...formdata,
+      [name]: value,
+    });
+
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("userdata",formdata);
+  }
+
+
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen bg-purple-300">
         <div>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="flex flex-col items-center justify-center">
               <div className="card-body gap-2">
+              <label className="label">
+                  <span className="label-text">Name</span>{" "}
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Enter your fullname"
+                  className="input"
+                  value={formdata.name}
+                  onChange={handleChange}
+                  
+                />
                 <label className="label">
                   <span className="label-text">Username</span>{" "}
                 </label>
                 <input
                   type="text"
+                  name="username"
                   placeholder="Enter your username"
                   className="input"
+                  value={formdata.username}
+                  onChange={handleChange}
                 />
                 <label className="label">
                   <span className="label-text">Email</span>{" "}
                 </label>
                 <input
                   type="email"
+                  name="email"
                   placeholder="Enter your email"
                   className="input"
+                  value={formdata.email}
+                  onChange={handleChange}
                 />
                 <label className="label">
                   <span className="label-text">Password</span>{" "}
                 </label>
                 <input
                   type="password"
+                  name="password"
                   placeholder="Enter your password"
                   className="input"
+                  value={formdata.password}
+                  onChange={handleChange}
                 />
               </div>
             </div>
             <div className="mt-4 flex justify-center">
-              <button className="btn btn-soft btn-primary">Sign Up</button>
+              <button type="submit" className="btn btn-soft btn-primary">Sign Up</button>
             </div>
           </form>
           <div className="text-center mt-4">
